ec85ec4ddbb983910bcc4d2391bc5a5e8e3cba74
Anna Smeaton <2263320s@student.gla.ac.uk>
2022-02-21 15:46:59 +0000

    base.html added with template files created but not filled in. URLs mapped.

--- iTECH_Group/wsgi.py
+++ iTECH_Group/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for studenteats project.
+WSGI config for iTECH_Group project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studenteats.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iTECH_Group.settings')
 
 application = get_wsgi_application()
---
+++ studenteats/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request): 
+    context_dict = {}
+    return render(request, 'studenteats/index.html', context=context_dict)
+
+def login(request): 
+    context_dict = {}
+    return render(request, 'studenteats/login.html', context=context_dict)
+
+def profile(request):
+    context_dict = {} 
+    return render(request, 'studenteats/profile.html', context=context_dict)
+
+def about(request):
+    context_dict = {}
+    return render(request, 'studenteats/about.html', context=context_dict)
+
+def restaurant(request): 
+    context_dict = {}
+    return render(request, 'studenteats/restaurant.html', context=context_dict)
+
+def recipe(request): 
+    context_dict = {}
+    return render(request, 'studenteats/recipe.html', context=context_dict)
+
+def forum(request): 
+    context_dict = {}
+    return render(request, 'studenteats/forum.html', context=context_dict)
