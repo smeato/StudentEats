9004d4dc2e29f87daf6c2dfa8af191b685bfdf0c
Abc56355875 <2612796z@student.gla.ac.uk>
2022-03-17 03:30:41 +0000

    add the discussion-detail page

--- studenteats/urls.py
+++ studenteats/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from studenteats import views 
+from studenteats import views
+from studenteats.models import Discussion_Replies 
 
 app_name = 'studenteats'
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('recipe/', views.recipe, name='recipe'),
     path('about', views.about, name='about'),
     path('forum/', views.forum, name='forum'),
-    path('help/', views.help, name='help')
+    path('help/', views.help, name='help'),
+    path('discussion_detail/<int:discussion_ID>',views.discussion_detail,name='discussion_detail')
 ]
