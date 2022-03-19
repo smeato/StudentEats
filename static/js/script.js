9b39a679e70c4fcf72401bad6e070229998f1a2a
Sharon Baby <sharon@Sharons-MacBook-Pro.local>
2022-03-19 03:04:52 +0000

    Map implementation: basics

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,65 @@ function recipeClick(){
     window.location.assign(url)
 }
 
+var inputMap, displayMap;
+
+function initMap() {
+    console.log('hello');
+    myLatLng = { lat: 34.397, lng: 150.644 }
+    displayMap = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 55.860916, lng: -4.251433 },
+        scrollwheel: false,
+        zoom: 8
+    });
+
+    const marker = new google.maps.Marker({
+        position: myLatLng,
+        displayMap,
+        title: "contentString",
+    });
+
+    const infowindow = new google.maps.InfoWindow({
+        content: "contentString",
+    });
+
+    marker.addListener("mouseover", () => {
+        infowindow.open({
+            anchor: marker,
+            displayMap,
+            shouldFocus: false,
+        });
+    });
+
+    marker.addListener('mouseout', () => {
+        infowindow.close();
+    });
+
+    marker.setMap(displayMap);
+
+    console.log('here');
+    myLatLng = { lat: 34.397, lng: 150.644 }
+    inputMap = new google.maps.Map(document.getElementById('test'), {
+        center: { lat: 34.397, lng: 150.644 },
+        scrollwheel: false,
+        zoom: 2
+    });
+
+    let infoWindow = new google.maps.InfoWindow({
+        content: "Click the map to get Lat/Lng!",
+        position: myLatLng,
+      });
+
+      infoWindow.open(inputMap);
+      inputMap.addListener("click", (mapsMouseEvent) => {
+        // Close the current InfoWindow.
+        infoWindow.close();
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+          position: mapsMouseEvent.latLng,
+        });
+        infoWindow.setContent(
+          JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(inputMap);
+      });
+}
--- studenteats/models.py
+++ studenteats/models.py
@@ -49,6 +49,9 @@ class Restaurant(models.Model):
     Owner=models.ForeignKey(User,on_delete=models.CASCADE)
     Res_Deals=models.ForeignKey(Deals, null=True, blank=True,on_delete=models.CASCADE)
     Likes=models.IntegerField(default=0)
+    Latitude = models.FloatField(default=0.0)
+    Longitude = models.FloatField(default=0.0)
+    Place = models.CharField(max_length=128, default='')
     
     def __str__(self): 
         return self.Name
